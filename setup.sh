d020dc4a25c92f3238f5bb9ba23204a7b1dcd103
crisdarbellay <133167509+crisdarbellay@users.noreply.github.com>
2024-10-26 16:59:33 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+user="${1}"
+if [[ "$user" == "" ]]; then
+    echo -e "\e[93;1mMust specify username\e[0m"
+    exit 1
+fi
+
+pip install --upgrade pip
+
+# Move into code directory
+cd pipeline_code
+
+# Miniconda3 ---------------------------------------------------------------------------------------------------------
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+echo -e "\e[91;1m│ Install conda to /home/$user/miniconda3 │\e[0m"
+bash Miniconda3-latest-Linux-x86_64.sh -b
+source /home/$user/miniconda3/etc/profile.d/conda.sh
+
+# Remove the install script
+rm -rf Miniconda3-latest-Linux-x86_64.sh
+
+conda init
+conda update conda
+
+# RFdiffusion ---------------------------------------------------------------------------------------------
+# Clone the git
+git clone https://github.com/RosettaCommons/RFdiffusion.git
+
+cd RFdiffusion
+mkdir models && cd models
+wget http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/f572d396fae9206628714fb2ce00f72e/Complex_beta_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/1befcb9b28e2f778f53d47f18b7597fa/RF_structure_prediction_weights.pt
+
+cd /home/$user/Protein-design-rcp/pipeline_code
+
+# Install the SE3 environment
+conda env create -f SE3nv-cuda11.7.yml
+conda activate SE3nv2.0
+
+# Add modules for the rest of the script to the env
+pip install spython yaml absl-py
+
+cd RFdiffusion/env/SE3Transformer
+pip install --no-cache-dir -r requirements.txt
+python setup.py install
+cd ../.. # change into the root directory of the repository
+pip install -e . # install the rfdiffusion module from the root of the repository
+
+cd /home/$user/Protein-design-rcp/pipeline_code
+
+# Protein MPNN -----------------------------------------------------------------------------------------
+git clone https://github.com/dauparas/ProteinMPNN.git
+
+# Prepare output folders
+cd ..
+mkdir Results
+mkdir AF_current_job
+
+# AlphaFold Installation --------------------------------------------------------------------------------
+# Clone AlphaFold repository
+git clone https://github.com/deepmind/alphafold.git /home/$user/Protein-design-rcp/alphafold
+
+# Set up AlphaFold dependencies
+cd /home/$user/Protein-design-rcp/alphafold
+pip install -r docker/requirements.txt
+
+# Create placeholders for database directories
+mkdir -p /home/$user/Protein-design-rcp/alphafold/params
+mkdir -p /home/$user/Protein-design-rcp/alphafold/databases
+
+# Set environment variables
+export ALPHAFOLD_DIR="/home/$user/Protein-design-rcp/alphafold"
+export ALPHAFOLD_DATADIR="/home/$user/Protein-design-rcp/alphafold/databases"
+
+echo "AlphaFold setup complete (databases not installed)."
+
+echo "FINISHED"
